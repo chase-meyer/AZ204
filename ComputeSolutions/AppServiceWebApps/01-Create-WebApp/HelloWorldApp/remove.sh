518fa706674c27de56e793061ef5d783742e3d5f
Chase Meyer <chase.e.meyer@gmail.com>
2025-02-26 20:03:11 +0000

    add remove.sh to remove resource group from azure

---
+++ ComputeSolutions/AppServiceWebApps/01-Create-WebApp/HelloWorldApp/remove.sh
@@ -0,0 +1,10 @@
+# ComputeSolutions/01-Create-WebApp/remove.sh
+#!/bin/bash
+
+# Name of the resource group to delete
+RESOURCE_GROUP="AZ204RG"
+
+# Delete the resource group and all associated resources
+az group delete --name $RESOURCE_GROUP --yes --no-wait
+
+echo "Resource group $RESOURCE_GROUP and all associated resources are being deleted."
